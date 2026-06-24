3e6506a732f625dbef4f380dffaeeb6f86d04602
hashirsajid58200p <hs58200p@gmail.com>
2026-06-24 19:03:29 +0500

    feat: navigate to home on logo click and retrieve speciality param in Doctors

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Doctors = () => {
+  const { speciality } = useParams();
+  console.log(speciality);
   return <div>Doctors</div>;
 };
 
